17bfb9b95752b5b5cb2f987ae2353465b47a3e66
eslamsabry15 <eslamsabry297@gmail.com>
2025-09-18 15:14:03 +0300

    Add VectorDBFactory "Qdrat"

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -13,7 +13,22 @@ class BaseModelController:
             'assets/files'
         )
 
+        self.database_dir = os.path.join(
+            self.base_dir,
+            "assets/database"
+        )
+
         
     def generate_random_key(self , length = 12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits , k = length))
+    
+
+    def get_database_path(self , db_name : str) :
+
+        database_path = os.path.join(self.database_dir , 
+                                     database_path)
         
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+
+        return database_path
---
+++ src/stores/vectorDB/providers/__init__.py
@@ -0,0 +1,1 @@
+from .QDProvider import QdrantDB
